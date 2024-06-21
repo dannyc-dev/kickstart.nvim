0f59312db9da993667f588374f44af0074c8bb53
dcolmenares <danny_colmenares@rapid7.com>
2024-06-21 10:57:30 -0400

    Parameterize

--- lua/config/dap-config.lua
+++ lua/config/dap-config.lua
@@ -4,12 +4,33 @@ local dap_python = require 'dap-python'
 dap_python.setup 'python'
 dap_python.test_runner = 'pytest'
 
+dap.configurations.python = {}
+table.insert(dap.configurations.python, {
+  type = 'python',
+  request = 'launch',
+  name = 'Launch Current File',
+  program = '${file}',
+})
+
 table.insert(dap.configurations.python, {
   type = 'python',
   request = 'launch',
   name = 'Pytest Current File',
   module = 'pytest',
   args = { '${file}', '-sv' },
+})
+
+table.insert(dap.configurations.python, {
+  name = 'Divvy: Mock Job',
+  type = 'python',
+  request = 'launch',
+  program = '${workspaceFolder}/Divvy/src/python/divvycloud.py',
+  args = function()
+    local user_input = vim.fn.input 'Mock Job: <Job name> <Job params>: '
+    local args = vim.split(user_input, ' ')
+    table.insert(args, 1, 'mock-job')
+    return args
+  end,
 })
 
 table.insert(dap.configurations.python, {
@@ -29,17 +50,15 @@ table.insert(dap.configurations.python, {
 })
 
 table.insert(dap.configurations.python, {
-  name = 'Divvy: Mock Job',
-  type = 'python',
-  request = 'launch',
-  program = '${workspaceFolder}/Divvy/src/python/divvycloud.py',
-  args = { 'mock-job', 'ContainerImageScanner', 'digest=sha256:d20b215bf2dbe28e3ea3760bab4af57b968150c6678699f0dffffe1118a48cdc', 'organizations=[1]' },
-})
-
-table.insert(dap.configurations.python, {
   type = 'python',
   request = 'launch',
   name = 'Divvy: Mock Harvester',
   program = '${workspaceFolder}/Divvy/src/python/divvycloud.py',
-  args = { 'mock-harvester', '--harvester', 'InstanceInterfaceHarvester' }, -- 'location', 'us-east-1'
+  args = function()
+    local user_input = vim.fn.input 'Mock Harvester: <Harvester name> <Harvester params>: '
+    local args = vim.split(user_input, ' ')
+    table.insert(args, 1, '--harvester')
+    table.insert(args, 1, 'mock-harvester')
+    return args
+  end,
 })
